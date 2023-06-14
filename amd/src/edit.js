d8b95f22c6866e1f12d95bf64f81ff1c0a747424
NinaHerrmann <nina.herrmann@uni-muenster.de>
2023-06-14 14:15:48 +0200

    amd build

--- amd/src/edit.js
+++ amd/src/edit.js
@@ -1,5 +1,3 @@
-import Templates from 'core/templates';
-import notification from 'core/notification';
 
 export const init = () => {
     document.getElementById('mootimeter_question').addEventListener('keyup', function () {
